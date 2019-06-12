9dc4471f0ed6c81c50e472ef95de982c4cc280c0
Rodrigo Rodrigues <rfr0067@hotmail.com>
2019-06-12 21:21:54 +0000

    test server

--- cardgame/program/main.cpp
+++ cardgame/program/main.cpp
@@ -1,5 +1,34 @@
 #include <iostream>
+#include <string>
+#include "httplib.h"
 
-int main() {
-    std::cout << "Hello Easy C++ project!" << std::endl;
+using namespace std;
+
+int main(void)
+{
+    using namespace httplib;
+
+    Server svr;
+    string camarada1;
+    string camarada2;
+
+    svr.Get("/hi", [](const Request& req, Response& res) {
+        res.set_content("Hello World!", "text/plain");
+    });
+    
+    
+    svr.Post("/join", [&](const Request& req, Response& res) {
+        if(camarada1.length() == 0){
+            camarada1 = req.get_param_value("nome", 0);
+        }
+        res.set_content(camarada1, "text/plain");
+    });
+
+
+    svr.Get(R"(/numbers/(\d+))", [&](const Request& req, Response& res) {
+        auto numbers = req.matches[1];
+        res.set_content(numbers, "text/plain");
+    });
+
+    svr.listen("localhost", 8080);
 }
