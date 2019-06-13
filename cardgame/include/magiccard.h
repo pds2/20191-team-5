2744788e63e8c7c38dbef94f82c183f813a59603
Alexandre Cesar <acmorcego@gmail.com>
2019-06-13 15:06:42 -0300

    implement apply for magic cards

--- cardgame/include/magiccard.h
+++ cardgame/include/magiccard.h
@@ -1,28 +1,29 @@
-#ifndef PDS2_MAGICCARD_H
-#define PDS2_MAGICCARD_H
+#pragma once
 
 #include "card.h"
 
 using namespace std;
 
-class MagicCard : public Card
-{
-private:
-    string description;
-    int attackDelta;
-    int defenseDelta;
-    int lifeDelta;
-    bool destroy;
-    Target target;
+class MagicCard : public Card {
+ private:
+  string description;
+  int attackDelta;
+  int defenseDelta;
+  int lifeDelta;
+  bool destroy;
+  Target target;
 
-public:
-    MagicCard();
-    MagicCard(string _description, int _attackDelta, int _defenseDelta, int _lifeDelta, bool _destroy, Target _target);
-    ~MargicCard();
+ public:
+  MagicCard();
+  MagicCard(string _description,
+            int _attackDelta,
+            int _defenseDelta,
+            int _lifeDelta,
+            bool _destroy,
+            Target _target);
+  ~MargicCard();
 
-    virtual void apply (Game _game, optional<Position> _position);
-    virtual Target getTarget();
-    virtual string getDescription();
-};  
-
-#endif /* PDS2_MAGICCARD_H */
+  virtual void apply(Game& game, optional<Position&> position);
+  virtual Target getTarget();
+  virtual string getDescription();
+};
