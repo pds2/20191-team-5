7970523046c86ddc9631e141e7738f29e9e3059e
jonas.trindade <jonastrinda@gmail.com>
2019-06-13 01:35:19 -0300

    Doxygen documentation

--- cardgame/include/turn.h
+++ cardgame/include/turn.h
@@ -11,11 +11,38 @@ public:
 	std::pair <bool, bool> attacked;
 
 private:
+	/**
+	* Points to the turn player
+	*/
 	Turn (int playerIndex);
+
+	/**
+	* Change of phase and if the last turn changes the turn
+	*/
 	void endPhase();
+
+	/**
+	* Returns the pahse the player is in the game
+	*/
 	Phase getPhase();
+
+	/**
+	* Returns the player's index of the run
+	*/
 	int getplayerIndex();
+
+	/**
+	* Returns the summoned monster card
+	*/
 	bool hasSummonedMonster();
+
+	/**
+	* Returns the summoned magic card
+	*/
 	bool hasSummonedMagic();
+
+	/**
+	* Returns the monster that attacked
+	*/
 	bool hasAttacked(int index);
 };
