9e3a04ba759d840986dd94fc7ac2440ff463a928
Alexandre Cesar <acmorcego@gmail.com>
2019-06-12 23:46:27 -0300

    implement monstercard apply

--- cardgame/include/monstercard.h
+++ cardgame/include/monstercard.h
@@ -1,26 +1,30 @@
-#ifndef PDS2_MONSTERCARD_H
-#define PDS2_MONSTERCARD_H
+#pragma once
 
-#include "card.h"
+#include <optional>
+
+#include <card.h>
+#include <mode.h>
 
 using namespace std;
 
-class MonsterCard : public Card
-{
-private:
-    int attack;
-    int defense;
-    Mode mode;
-public:
-    MonsterCard();
-    MonsterCard(string _name, int _attack, int _defense);
-    ~MonsterCard();
+class MonsterCard : public Card {
+ private:
+  int attack;
+  int defense;
+  Mode mode;
 
-    Mode toggleMode();
-    int getAttack();
-    int getDefense();
-    Mode getMode();
+  void removeFrom(std::pair<std::optional<MonsterCard*>,
+                            std::optional<MonsterCard*>>& field);
 
+ public:
+  MonsterCard();
+  MonsterCard(string _name, int _attack, int _defense);
+  ~MonsterCard();
+
+  Mode toggleMode();
+  int getAttack();
+  int getDefense();
+  Mode getMode();
+
+  virtual void apply(Game& game, std::optional<Position&> position) override;
 };
-
-#endif /* PDS2_MonsterCard_H */
