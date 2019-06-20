515c6139ac9dcc27377e3c70697c7764332677ee
Jonas Trindade <jonastrinda@gmail.com>
2019-06-20 20:53:32 -0300

    Add files via upload

---
+++ teste_magiccard.cpp
@@ -0,0 +1,16 @@
+#include "doctest.h"
+#include "magiccard.h"
+
+#define NAME "CARTA"
+#define DESCRIPTION "DESC"
+#define ATTACK 300
+#define DEFENSE 300
+#define LIFE 2000
+#define DESTROY true
+
+TEST_CASE("Teste 1 - MagicCard")
+{
+	MagidCard card = MagicCard(NAME, DESCRIPTION, ATACK, DEFENSE, LIFE, DESTROY);
+	CHECK_EQ(card.isSummonable(), true);
+	CHECK_EQ(card.getDescription(), DESC);
+}
